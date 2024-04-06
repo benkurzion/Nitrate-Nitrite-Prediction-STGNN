288fc393c0c8563fc8c7770f1d2ecf1e9751a95f
benkurzion <benkurzion@gmail.com>
2024-04-05 21:48:05 -0400

    initial work

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 import os
-import datetime
-
-import IPython
-import IPython.display
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+import torch
+import tsl
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from tsl.datasets import MetrLA
 from utils import *
 
+# Presets (per TSL Ninjas)
+pd.options.display.float_format = '{:.2f}'.format
+np.set_printoptions(edgeitems=3, precision=3)
+torch.set_printoptions(edgeitems=2, precision=3)
+
+
+
+dataset = MetrLA(root='./data')
+print(dataset)
+print(f"Sampling period: {dataset.freq}")
+print(f"Has missing values: {dataset.has_mask}")
+print(f"Percentage of missing values: {(1 - dataset.mask.mean()) * 100:.2f}%")
+print(f"Has exogenous variables: {dataset.has_covariates}")
+print(f"Covariates: {', '.join(dataset.covariates.keys())}")
+dataset.dataframe()
+
+connectivity = dataset.get_connectivity(threshold=0.1, include_self=False, normalize_axis=1, layout="edge_index")
+print("connectivity = \n", connectivity)
+
+edge_index, edge_weight = connectivity
+
+print(f'edge_index {edge_index.shape}:\n', edge_index)
+print(f'edge_weight {edge_weight.shape}:\n', edge_weight)
 '''
-Training set = 70%
-Validation set = 20%
-Test set = 10%
+Data contains time series for __ sensors from March 6th, 2021 to March 1st, 2022. 
+Each day contains 48 (plus or minus 1 or 2) readings from any of these sensors!
+
+We are using March 6th, 2021 --> April 31st, 2021 as training data
+We are using March 1st, 2023 as testing data
 '''
 
-#TO BE FILLED IN IN SOME CAPACITY
-dataFeatures = [] 
-dataLables = []
+# Load data
+fullData = pd.read_csv("waterflowdataset.txt", sep=",")
+adjMatrix = pd.read_csv("adjmatrix.txt", sep=",", header=None)
 
-trainFeatures, _, trainLabels, _ = train_test_split(X=dataFeatures, y=dataLables, test_size=0.7, random_state=1)
-validationFeatures, testFeatures, validationLables, testLables = train_test_split(X=trainFeatures, y=trainLabels, test_size=0.66, random_state=1)
-
-#Normalize data
-scaler = StandardScaler()
-trainFeatures = scaler.fit_transform(trainFeatures)
-validationFeatures = scaler.transform(validationFeatures)
-testFeatures = scaler.transform(testFeatures)
+# Split data to features and label
+labels = fullData["nitrite+nitrate"]
+features = fullData[["site_no","dateTime","water discharge","latitude","longitude","altitude"]]
 
 
+'''TODO: rework the adj matrix such that it is two array of length E number of edges
+        array[0][i] = source, array[1][i] = sink for every edge i in the graph
+        -> and a second array weights of length E number of edges
+        weights[i] = weight of edge i for all i
+        
+        Figure out how the data should be formatted in relation to:
+            1) where do the features go?
+            2) where do the labels go?
 '''
-Input width: how many samples are we looking back in time for prediction
-    Each of our samples account for 15 minutes. So input width of 4 means training on the previous hour
-Offset (shift): How far into the future we want our prediction to be
-Label Width: How many time steps we want to predict
-'''
-
-
-window = WindowGenerator(input_width=20, label_width=4, shift=1, label_columns=['Nitrate/Nitrite Concentration'])
-window.split_window = split_window
