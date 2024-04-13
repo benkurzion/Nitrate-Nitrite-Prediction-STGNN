c163478dea716219b0fb1ced2513791ee4a5133f
benkurzion <benkurzion@gmail.com>
2024-04-13 14:35:24 -0400

    plot functionality and bug fix

--- autoregressive.py
+++ autoregressive.py
@@ -3,7 +3,7 @@ from statsmodels.tsa.ar_model import AutoReg
 import numpy as np
 import pandas as pd
 import torch
-
+import matplotlib.pyplot as plt
 
 PATH_TO_NODE_FEATURES = "node_features.txt"
 
@@ -21,43 +21,62 @@ for i in range (fullData.shape[0]):
         currentNodeFeatures = []
         nodeByNodeLabels.append(currentNodeLabels)
         currentNodeLabels = []
-    currentNodeFeatures.append(fullData.iloc[i, [3,5,6,7]].to_numpy(dtype=float))
+    currentNodeFeatures.append(fullData.iloc[i, [3]].to_numpy(dtype=float))
     currentNodeLabels.append(fullData.iloc[i, 4])
 
-predictions = [] 
+predictions = []
 testLabels = []
 # Train autoregressive model on each node
 for i in range(len(nodeByNodeFeatures)):
     #grab the last day as the test day
-    testFeatures = nodeByNodeFeatures[i][-1]
-    testFeatures = np.array([testFeatures])
+    testFeatures = nodeByNodeFeatures[i][-3:]
+    #testFeatures = np.array([testFeatures])
     #remove the test data
-    nodeByNodeFeatures[i] = nodeByNodeFeatures[i][:-1]
+    nodeByNodeFeatures[i] = nodeByNodeFeatures[i][:-3]
 
     #grab the last label as the test label
-    testLabel = nodeByNodeLabels[i][-1] 
+    testLabel = nodeByNodeLabels[i][-3:]
     testLabels.append(testLabel)
     #remove the test label
-    nodeByNodeLabels[i] = nodeByNodeLabels[i][:-1]
+    nodeByNodeLabels[i] = nodeByNodeLabels[i][:-3]
 
     #fit the model with the training days
     model = AutoReg(endog=nodeByNodeLabels[i], exog=nodeByNodeFeatures[i], lags=1)
     model = model.fit()
 
     #predict the test day
-    predictions.append(model.predict(start=len(nodeByNodeFeatures[i]), end=len(nodeByNodeFeatures[i]), exog_oos=testFeatures)[0]) #predicting one step into the future
+    predictions.append(model.predict(start=len(nodeByNodeFeatures[i]), end=len(nodeByNodeFeatures[i]) + 2, exog_oos=testFeatures)) #predicting 3 steps into the future
 
-print(predictions) 
 
-def metrics():
-    # mae of 0.164
-    print(np.mean(np.abs(np.array(predictions)-np.array(testLabels))))
+# MAE
+print("MAE = ")
+print(np.mean(np.abs(np.array(predictions)-np.array(testLabels))))
 
-    # mean squared error of 0.04
-    print(np.mean(np.square(np.array(predictions)-np.array(testLabels))))
+# mean squared error
+print("\nMSE = ")
+print(np.mean(np.square(np.array(predictions)-np.array(testLabels))))
 
-    # mean absolute percentage error
-    sum_term= 0;
-    for i in range (len(testLabels)):
-      sum_term+= np.abs((testLabels[i]- predictions[i])/testLabels[i])
-    print(sum_term/len(testLabels)*100)
+# mean absolute percentage error
+sum_term= 0;
+for i in range (len(testLabels)):
+    for day in range (len (testLabels[0])):
+        sum_term+= np.abs((testLabels[i][day]- predictions[i][day])/testLabels[i][day])
+print("\nMAPE = ")
+print(sum_term/len(testLabels)*100)
+
+# create the scatter plot visual for the labels vs predictions for the third day
+'''y = []
+for i in range (len(testLabels)):
+    y.append(testLabels[i][2])
+yhat = []
+for i in range (len(predictions)):
+    yhat.append(predictions[i][2])
+x = range(len(testLabels))
+
+plt.scatter(x, y, label='Labels')
+plt.scatter(x,yhat, label='Predictions')
+plt.xlabel('Node')
+plt.ylabel('nitrate/nitrite concentration')
+plt.title('Autoregressive predictions for each node')
+plt.legend()
+plt.show()'''
